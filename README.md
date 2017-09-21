76b0fbcd7d3c2db8109b04a1187a97ab1ac90603
joncancode <jc.haines19@gmail.com>
2017-09-21 00:01:21 -0400

    chapter one runs completely with limited battle system, including a Pikachu Easter egg

---
+++ README.md
@@ -0,0 +1,46 @@
+# Pokemon Python
+
+## A console application
+
+As a guy who grew up in the Nintendo era, video games always fascinated me. This, and the fact that my parents were always tell me to read a book, led me to an interest in choose your own adventure games as well. 
+
+To combine my love for both, I've recreated the first part of the original Pokemon Gameboy game into a choose your own adventure for the console. It's short at the moment, but I had so much fun with it, I may try to do a more complex version in the future.
+
+## Features
+
+You're able to choose your own Pokemon, each with its own abilities. There is even an easter egg to get Pikachu.
+
+## Tech use
+
+This uses strictly Python.
+
+##See it in action
+
+![Pokemon screenshot](/images/poke-python.gif?raw=true "Pokemon")
+
+##Try it yourself
+
+Clone this repository 
+
+``` git clone https://github.com/joncancode/console-adventure.git```
+
+In order to run this, it would be best to create a virtual environment.
+
+```python3 -m venv env```
+
+```mkdir adventure```
+
+```cd adventure```
+
+get started with git
+```git init```
+
+```adventure/env```
+
+```echo env >> .gitignore```
+
+activate the environment any time you want to play
+```source env/bin/activate```
+
+then deactivate in the console when you are done
+```deactivate```
--- game.py
+++ game.py
@@ -23,16 +23,18 @@ print("""\
      .   ,                   |                       
       ._     '           _'  |                    
       `.`-...___,...---""    |       
-       - .`._        _,-,.'   .                
+          `._        _,-,.'                   
 
-Made in \033[1;32mPython\033[0m to feed your nostalgia!                   
+Made in \033[1;33mPython\033[0m to feed your nostalgia!                   
                     """)
 
 time.sleep(1)
 
+def main():
+    startGame()
+
 def startGame():
     input('Press enter to begin...')
-startGame()
 
 myName = input('\nWhat\'s your name? ')
 pikachu = '\033[1;33mPikachu\033[0m'
@@ -73,6 +75,45 @@ elif dayOfWeek == 6:
 
 #
 
+
+def lose():
+    print("""\
+
+    \t*****  \033[1;33mGAME OVER\033[0m  *****
+
+    """)
+
+def leaveTown(myPokemon):
+    time.sleep(1)
+    print("\033[2J\033[0;0H")
+    print(f"You and {myPokemon} leave Pallet Town to start the real adventure")
+    print("""\
+
+        \t*****  \033[1;34mEnd of Chapter 1\033[0m  *****
+
+      """)
+
+def winBattle(myPokemon, garyPokemon):
+    time.sleep(1)
+    print(f"{newL}{myPokemon} is victorious!{newL}")
+    time.sleep(1)
+    print(f"Gary and his {garyPokemon} leave upset. The professor congratulates you and you are on your way to become a master.{newL}")
+    time.sleep(1)
+    input(f"Press enter to continue...")
+    leaveTown(myPokemon)
+
+def loseBattle(myPokemon, garyPokemon):
+    time.sleep(1)
+    print(f"{newL}Gary's {garyPokemon} counters with a Body Slam, causing {myPokemon} to faint. You lose!{newL}")
+    time.sleep(1)
+    print(f"Gary and his {garyPokemon} leave happily. The professor congratulates you and you are on your way to become a master.{newL}")
+    time.sleep(1)
+    print(f"Oak: You have a long way to go {myName}, but keep training. You will one day be a master.{newL}")
+    time.sleep(1)
+    input(f"Press enter to continue...")
+    leaveTown(myPokemon)
+
+
 def garyBattle(myPokemon, garyPokemon):
     if myPokemon == bulbasaur:
         specialMove = 'Vine Whip'
@@ -93,18 +134,22 @@ def garyBattle(myPokemon, garyPokemon):
     time.sleep(1)
     firstAttack = input(f'{choicePrompt} {newL} {choice1} Tackle {newL} {choice2} Growl {newL} {choice3} {specialMove} {newL} {choice4} {specialMove2} {newL}' )
     if firstAttack == '1':
-        print(f"{myPokemon} tackles Gary's {garyPokemon} and inflicts some damage")
+        print(f"{myPokemon} tackles Gary's {garyPokemon} and inflicts damage.")
+        winBattle(myPokemon, garyPokemon)
     if firstAttack == '2':
-        print(f"{myPokemon} 2nd attack Gary's {garyPokemon} and inflicts some damage")
+        print(f"{myPokemon} growls at Gary's {garyPokemon}.")
+        loseBattle(myPokemon, garyPokemon)
     if firstAttack == '3':
-        print(f"{myPokemon} 3rd attack Gary's {garyPokemon} and inflicts some damage")
+        print(f"{myPokemon} uses {specialMove} on Gary's {garyPokemon}. It's not very effective.")
+        loseBattle(myPokemon, garyPokemon)
     if firstAttack == '4':
-        print(f"{myPokemon} yawns at Gary's {garyPokemon}. It's not very effective")
+        print(f"{myPokemon} uses {specialMove2} on Gary's {garyPokemon}. It's super effective.")
+        winBattle(myPokemon, garyPokemon)
 
 #Choice 5: Gary's Challenge
 def garyChallenge(myPokemon, garyPokemon):
     time.sleep(1)
-    print(f'Before I go become the best trainer around, I want my {garyPokemon} to take out your puny {myPokemon}. Ready, {myName}?')
+    print(f'Gary: Before I go become the best trainer around, I want my {garyPokemon} to take out your puny {myPokemon}. Ready, {myName}?')
     time.sleep(1)
     input(f"{newL}Press enter to continue...")
     garyBattle(myPokemon, garyPokemon)
@@ -139,23 +184,41 @@ def choosePokemon(myPokemon):
     garyChooses(myPokemon, garyPokemon)
 
 #Choice 2: The Lab
+def lateLabAsh():
+    print("\033[2J\033[0;0H")
+    print('\nYou arrive at the lab and find the Professor standing by himself\n')
+    time.sleep(1)
+    print(f'Oak: What took you so long, {myName}?!')
+    time.sleep(1)
+    print(f"{newL}The three main Pokemon are gone. No more {bulbasaur}, {charmander}, or {squirtle}.")
+    time.sleep(2)
+    print('\nBut there is another...\n')
+    time.sleep(1)
+    print(f'All of a sudden, there is a {pikachu} sitting next to you.')
+    myPokemon = pikachu
+    input(f"{newL}Press enter to continue...")
+    leaveTown(myPokemon)
+
 def lateLab():
     print("\033[2J\033[0;0H")
     print('\nYou arrive at the lab and find the Professor standing by himself\n')
     time.sleep(1)
     print(f'Oak: What took you so long {myName}!')
     time.sleep(1)
-    print(f"{newL}The three main Pokemon are gone. No more {bulbasaur}, {charmander}, or {squirtle}")
+    print(f"{newL}The three main Pokemon are gone. No more {bulbasaur}, {charmander}, or {squirtle}.")
     time.sleep(2)
-    print('\nBut there is another...\n')
+    print(f'{newL}Sorry... You lose.{newL}')
     time.sleep(1)
-    print(f'All of a sudden, there is a {pikachu} sitting next to you.')
-
+    input(f"{newL}Press enter to continue...")
+    lose()
 
 def labStart():
-    print('\nYou arrive at the lab and find the professor standing with his grandson.\n')
+    print(f'{newL}You arrive at the lab and find the professor standing with his grandson.{newL}')
     time.sleep(1)
-    print(f'Gary: Look who finally showed up. Hurry up and choose, {myName}.')
+    print(f'Gary: Look who finally showed up. Hurry up and choose, {myName}.{newL}')
+    time.sleep(1)
+    input(f"Press enter to continue...")
+    print("\033[2J\033[0;0H")
     time.sleep(1)
     print('\nYou walk up to the professor\'s counter. Whom will you choose?\n')
     time.sleep(2)   
@@ -204,7 +267,10 @@ def staySleep2():
         time.sleep(1)
         print('THE PROFESSOR!!! \n')
         print('You spring up from your bed and run to the lab in your pajamas')
-        lateLab()
+        if myName == 'Ash':
+            lateLabAsh()
+        if myName != 'Ash':
+            lateLab()
 
 def staySleep1():
     print("\033[2J\033[0;0H")
@@ -224,6 +290,9 @@ elif wakeUpResponse == '2':
     time.sleep(1)
     print('few more minutesszzzZzzZ')
     staySleep1()
+
+
+main()
 
 ######################################
 
